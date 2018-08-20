9b5ce6b8b589ab73fab48833bc178ce77f128232
daksh <daksh.sagar@yahoo.com>
2018-08-20 18:56:12 +0530

    Refactor project code

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+//Routes middleware
+app.use('/auth/google', authRouter);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
