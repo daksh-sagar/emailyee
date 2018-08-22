ca163038fc446745038469bc37524387eebecda8
daksh <daksh.sagar@yahoo.com>
2018-08-22 12:26:25 +0530

    Add prod keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   //we are in production return prod set of keys
+  module.exports = require('./prod');
 } else {
   //return dev keys
   module.exports = require('./dev');
