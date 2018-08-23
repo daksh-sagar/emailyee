67d49044f8f5f920fc4d4ed311f6eda5b2052f44
daksh <daksh.sagar@yahoo.com>
2018-08-24 03:23:08 +0530

    maybe a fix

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use('/auth', authRouter);
 app.use('/api', billingRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/build/'));
 
   const path = require('path');
   app.get('*', (req, res) => {
