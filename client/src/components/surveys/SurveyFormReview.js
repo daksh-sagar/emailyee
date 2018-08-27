3f59402cc7f4abe9759e2504b3ce0eb0b1551633
daksh <daksh.sagar@yahoo.com>
2018-08-27 19:12:20 +0530

    Redirect after email is sent

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import _ from 'lodash';
 import formFields from './formFields';
-import * as actions from '../../actions/index'
+import * as actions from '../../actions/index';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const fields = _.map(formFields, field => {
     return (
       <div key={field.name}>
@@ -18,10 +19,16 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
       <h5>Please confirm your entries</h5>
       {fields}
       <div />
-      <button onClick={onCancel} className="yellow darken-3 btn-flat white-text">
+      <button
+        onClick={onCancel}
+        className="yellow darken-3 btn-flat white-text"
+      >
         Go back
       </button>
-      <button className="btn-flat green right" onClick={() => submitSurvey(formValues)}>
+      <button
+        className="btn-flat green right"
+        onClick={() => submitSurvey(formValues, history)}
+      >
         Send Survey <i className="material-icons right white-text">email</i>{' '}
       </button>
     </div>
@@ -32,4 +39,7 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(
+  mapStateToProps,
+  actions
+)(withRouter(SurveyFormReview));
