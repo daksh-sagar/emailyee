648ea9e8df05c14927c84ecd1b53b0733568fbd7
daksh <daksh.sagar@yahoo.com>
2018-08-28 04:03:39 +0530

    Show surveys on the frontend

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 class Dashboard extends Component {
   render() {
-    return <div>Dashboard
+    return <div>
+      <SurveyList/>
       <div className="fixed-action-btn">
         <Link className="btn-floating btn-large red" to="/surveys/new">
           <i className="large material-icons">add</i>
